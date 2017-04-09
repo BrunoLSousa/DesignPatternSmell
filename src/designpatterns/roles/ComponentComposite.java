ebd5ab4306bf2731321b01fda185dcb94f68d24a
Bruno Sousa <bruno.luan.sousa@gmail.com>
2017-04-09 15:27:47 -0300

    Representation structure of existing roles within the Composite design pattern.

---
+++ src/designpatterns/roles/ComponentComposite.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpatterns.roles;
+
+import designpatterns.structure.Type;
+
+/**
+ *
+ * @author bruno
+ */
+public class ComponentComposite extends Type{
+
+    private Composite composite;
+    
+    public ComponentComposite(String completeName) {
+        super(completeName);
+    }
+    
+    public Composite getComposite(){
+        return this.composite;
+    }
+    
+    public void setComposite(Composite composite){
+        this.composite = composite;
+    }
+}
---
+++ src/designpatterns/roles/Composite.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpatterns.roles;
+
+import designpatterns.structure.Attribute;
+import designpatterns.structure.Method;
+import designpatterns.structure.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author bruno
+ */
+public class Composite extends Type{
+
+    private Attribute component;
+    private List<Method> operations;
+    
+    public Composite(String completeName) {
+        super(completeName);
+        this.operations = new ArrayList<>();
+    }
+    
+    public Attribute getAttribute(){
+        return this.component;
+    }
+    
+    public List<Method> getOperations(){
+        return this.operations;
+    }
+    
+    public Method getOperationByIndex(int index){
+        return this.operations.get(index);
+    }
+    
+    public void addOperation(Method operation){
+        this.operations.add(operation);
+    }
+    
+    public void setAttribute(Attribute component){
+        this.component = component;
+    }
+
+    @Override
+    public String toString() {
+        return "Composite";
+    }
+    
+}
