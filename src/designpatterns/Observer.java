4e30a8ef460142de9720ff9b0294a791ca99b624
Bruno Sousa <bruno.luan.sousa@gmail.com>
2017-04-09 19:08:23 -0300

    Representation structure of Observer design pattern.

---
+++ src/designpatterns/Observer.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpatterns;
+
+import designpatterns.roles.ObserverRole;
+import designpatterns.roles.SubjectObserver;
+import designpatterns.structure.Method;
+import designpatterns.structure.MethodBadSmell;
+import designpatterns.structure.Statement;
+import designpatterns.structure.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import org.jdom2.Element;
+
+/**
+ *
+ * @author bruno
+ */
+public class Observer extends DesignPattern {
+
+    private List<SubjectObserver> subjects;
+
+    public Observer(String name) {
+        super(name);
+        this.subjects = new ArrayList<>();
+    }
+
+    @Override
+    public void countInstaces(Element element) {
+        List<Element> elements = element.getChildren();
+        for (Element e : elements) {
+            addInstance();
+        }
+    }
+
+    @Override
+    public Element convertElement(Element element) {
+        List<Element> elements = element.getChildren();
+        for (Element e : elements) {
+            addInstance();
+            List<Element> roles = e.getChildren();
+            int indexSuject = -1;
+            ObserverRole observer = null;
+            for (Element r : roles) {
+                if (r.getAttributeValue("name").equals("Observer")) {
+                    observer = addObserver(r.getAttributeValue("element"));
+                    addClass();
+                } else if (r.getAttributeValue("name").equals("Subject")) {
+                    indexSuject = addSubject(r.getAttributeValue("element"), observer);
+                } else if (r.getAttributeValue("name").equals("Notify()")) {
+                    addNotify(r.getAttributeValue("element"), indexSuject, r.getAttributeValue("name"));
+                    addMethod();
+                }
+            }
+        }
+        return element;
+    }
+
+    private ObserverRole addObserver(String observer) {
+        ObserverRole newObserver = new ObserverRole(observer);
+        return newObserver;
+    }
+
+    private int addSubject(String subject, ObserverRole observer) {
+        SubjectObserver newSubject = new SubjectObserver(subject);
+        int index = 0;
+        for (SubjectObserver s : this.subjects) {
+            if (s.getName().equals(newSubject.getName()) && s.getPack().equals(newSubject.getPack())) {
+                s.addObserver(observer);
+                return index;
+            }
+            index++;
+        }
+        newSubject.addObserver(observer);
+        this.subjects.add(newSubject);
+        addClass();
+        return index;
+    }
+
+    private void addNotify(String notify, int indexSubject, String roleDesignPattern) {
+        Statement newNotify = new Method(notify, roleDesignPattern);
+        this.subjects.get(indexSubject).addNotify((Method) newNotify);
+    }
+
+}
